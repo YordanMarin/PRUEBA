5743201b45e125e84681c73bdbb09f729472c4a7
yordan <yordan@gmail.com>
2025-03-27 12:16:42 -0500

    subida 1

---
+++ ejer1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi primera subida a GitHub");
+            Console.ReadKey();
+        }
+    }
+}
